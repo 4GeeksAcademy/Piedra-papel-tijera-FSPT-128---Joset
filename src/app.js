378d0b9c463e614b0eccdffade25342eebb8991b
Joset Santiago <josetsinph@gmail.com>
2025-11-22 18:56:59 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+const choice = ["piedra", "papel", "tijeras", "lagarto", "spock"];
+const rules = {
+  piedra: ["tijeras", "lagarto"],
+  papel: ["piedra", "spock"],
+  tijeras: ["papel", "lagarto"],
+  lagarto: ["spock", "papel"],
+  spock: ["tijeras", "piedra"]
+}
 
+const jugar = (userChoice) => {
+  const sheldonChoice = choice[Math.floor(Math.random() * choice.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  console.log(`Tú elegiste: ${userChoice}`);
+  console.log(`Sheldon eligió: ${sheldonChoice}`);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  if (userChoice === sheldonChoice) {
+    console.log("¡Empate!, prueba otra vez.");
+  } else if (rules[userChoice].includes(sheldonChoice)) {
+    console.log("¡Muy bien jugado, eres el campeón!");
+  } else {
+    console.log("¡perdiste contra Sheldon!");
+  }
+}
+
